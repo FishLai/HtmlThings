e8b9f150275e6c16f8be80cde69b7d30be32206d
FishLai <38682183+FishLai@users.noreply.github.com>
2021-01-23 00:32:03 +0800

    new js for typewriter

---
+++ My Info Card/brief intro 03/typing-writer.js
@@ -0,0 +1,11 @@
+function typewriter(el) {
+	text = el.innerText;
+	el.innerText = "";
+	node = document.createElement('span');
+	node.classList.add('blink-cursor');
+	
+}
+
+
+var el = document.querySelector('p');
+
