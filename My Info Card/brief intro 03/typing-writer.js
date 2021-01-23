66559139f4266f81a6c52581cf56530e671bb488
FishLai <38682183+FishLai@users.noreply.github.com>
2021-01-23 10:50:50 +0800

    testing typewriter effect

--- My Info Card/brief intro 03/typing-writer.js
+++ My Info Card/brief intro 03/typing-writer.js
@@ -1,12 +1,34 @@
+
+function typingWord(text, selector_string, i) {
+	if(i<text.length){
+		blinkcursor = document.querySelector(selector_string);
+		word = text[i];
+		blinkcursor.insertAdjacentText('beforebegin', word);
+		i++;
+		setTimeout(typingWord(text, selector_string, i), 300);
+	}
+}
+
+function typingLine(split_text, selector_string, j) {
+	if(j<split_text.length) {
+		text = split_text[j].valueOf();
+		typingWord(text, selector_string, 0);
+		blinkcursor = document.querySelector(selector_string);
+		el = document.createElement('br');
+		blinkcursor.insertAdjacentElement('beforebegin', el);
+		j++;
+		setTimeout(typingLine(split_text, selector_string, j), 10000);
+	}
+}
+
 function typewriter(el) {
 	text = el.innerText;
-	el.innerText = "";
-	node = document.createElement('span');
-	node.classList.add('blink-cursor');
-	el.appendChild(node);
-	
+	split_text = text.split('\n');
+	el.innerHTML = '<span class="blink-cursor"></span>';
+	selector_string = 'span.blink-cursor';
+	typingLine(split_text, selector_string, 0);
 }
 
 
 var el = document.querySelector('p');
-
+typewriter(el);
