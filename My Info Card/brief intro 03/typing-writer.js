f3ae1a1a4f4c33a2033a053bce77d7ac78a1ea85
FishLai <quan_yu_lai@outlook.com>
2021-01-23 18:03:01 +0800

    done typing effect

--- My Info Card/brief intro 03/typing-writer.js
+++ My Info Card/brief intro 03/typing-writer.js
@@ -1,34 +1,33 @@
-
-function typingWord(text, selector_string, i) {
-	if(i<text.length){
-		blinkcursor = document.querySelector(selector_string);
-		word = text[i];
-		blinkcursor.insertAdjacentText('beforebegin', word);
+function typewriter(i, j, delay) {
+	setTimeout(function(){
+		line = split_text[j];
+		word = line[i];
+		cursor.insertAdjacentText('beforebegin', word);
 		i++;
-		setTimeout(typingWord(text, selector_string, i), 300);
-	}
+		if( i<line.length ) {
+			typewriter(i, j, 50);
+		} else {
+			breakline = document.createElement('br');
+			cursor.insertAdjacentElement('beforebegin', breakline);
+			i = 0;
+			j++;
+			if( j<split_text.length ) {
+				typewriter(i, j, 700);
+			}
+		}
+	}, delay);
 }
 
-function typingLine(split_text, selector_string, j) {
-	if(j<split_text.length) {
-		text = split_text[j].valueOf();
-		typingWord(text, selector_string, 0);
-		blinkcursor = document.querySelector(selector_string);
-		el = document.createElement('br');
-		blinkcursor.insertAdjacentElement('beforebegin', el);
-		j++;
-		setTimeout(typingLine(split_text, selector_string, j), 10000);
-	}
-}
-
-function typewriter(el) {
+function typewriterinitial(el) {
 	text = el.innerText;
 	split_text = text.split('\n');
 	el.innerHTML = '<span class="blink-cursor"></span>';
 	selector_string = 'span.blink-cursor';
-	typingLine(split_text, selector_string, 0);
+	cursor = document.querySelector(selector_string);
+	typewriter(0, 0, 200);
 }
 
 
 var el = document.querySelector('p');
-typewriter(el);
+setTimeout(function(){ typewriterinitial(el)}, 2500);
+
